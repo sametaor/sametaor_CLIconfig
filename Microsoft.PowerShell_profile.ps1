1c41060083983db777e8f5080dafed1cfed4e0f4
sametaor <71749831+sametaor@users.noreply.github.com>
2024-05-28 16:01:25 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -123,3 +123,5 @@ Set-PSReadLineOption -EditMode Windows
 $env:PATH += ";$env:LOCALAPPDATA\MEGAcmd"
 mega-webdav MegaDrive
 echo "Commonly used commands:","cd","ls" "lt","ep","clear","winget","wsl","spicetify","choco","pip","git","gh","neofetch-win","py","sfc /scannow"
+echo "Checking for Updates..."
+winget upgrade --all --include-unknown
