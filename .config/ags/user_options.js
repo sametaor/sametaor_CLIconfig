a423546148de9c3fb56d886768af29f4c6e68dd3
sametaor <71749831+sametaor@users.noreply.github.com>
2024-08-08 21:25:31 +0530

    Added modified end-4 ags configs

---
+++ .config/ags/user_options.js
@@ -0,0 +1,12 @@
+// For every option, see ~/.config/ags/modules/.configuration/user_options.js
+// (vscode users ctrl+click this: file://./modules/.configuration/user_options.js)
+// (vim users: `:vsp` to split window, move cursor to this path, press `gf`. `Ctrl-w` twice to switch between)
+//   options listed in this file will override the default ones in the above file
+
+const userConfigOptions = {
+  'appearance': {
+    'fakeScreenRounding' : 0, // 0: None | 1: Always | 2: When not fullscreen
+  },
+}
+
+export default userConfigOptions;
