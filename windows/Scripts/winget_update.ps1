ae5d33ad249830db48b54be038b4d6a606a66c97
sametaor <71749831+sametaor@users.noreply.github.com>
2026-08-18 08:06:30 +0530

    Removed Windows+Random commits

--- windows/Scripts/winget_update.ps1
+++
@@ -1,6 +0,0 @@
-# File: Update-Winget-Apps.ps1
-Set-ExecutionPolicy Unrestricted CurrentUser
-echo "Checking for upgrades..."
-winget upgrade --all --accept-source-agreements --accept-package-agreements --include-pinned
-echo "Checking for Chocolatey upgrades..."
-choco upgrade all -y -i
